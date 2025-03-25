d58538edcf9c3d297c9135d9b387e29e0b1f5809
Katherine <kcicerov@ufpso.edu.co>
2025-03-24 20:47:53 -0500

    actualizado

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
         int i;
 
         // Solicitar al usuario un valor para la variable
-        System.out.print("Introduce un número a ver si lo adivinas: ");
+        System.out.println("Introduce un número a ver si lo adivinas: ");
         i = scanner.nextInt();
 
         // Estructura if-else para comparar el valor de 'i'
         if (i == 0 || i == 6 || i == 8) {
             System.out.print("adivinaste");
         } else {
-            System.out.println("No tienes adivinaste");
+            System.out.println("No adivinaste");
         }
 
         scanner.close(); // Cerramos el scanner para evitar fugas de recursos
